4193f0ad2d359588574940a976dbe92b595029be
Rhyality <ryanrheaume15@gmail.com>
2023-05-16 14:29:51 -0400

    hi*

--- data.py
+++ data.py
@@ -3,24 +3,21 @@ WEBSITE_LIST = {"https://www.youtube.com/":
                  "main_menu":'refresh_sens:id;guide-icon',
                  "endpoints":
                  { 
-                  "login-page":   'direct-link;signin',
-                  "video":  'css selector;ytd-rich-item-renderer'
+                  "membership":   'direct-link;signin',
+                  "careers":  'css selector;ytd-rich-item-renderer'
                  },
                   "sub-endpoints":
                   {
-                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
-                                   "music":   'relies_prev:partial link text;Music',
-                                   "gaming":  'relies_prev:partial link text;Gaming',
-                                   "news":    'relies_prev:partial link text;News',
-                                   "sports":  'relies_prev:partial link text;Sports',
-                                   "learning":'relies_prev:partial link text;Learning'
+                      "membership":{"price":'partial link text;What currency is the pricing in',
                                    },
                       "trending":{"video":'rand_ind:css selector;ytd-video-renderer'
                                   },
                       "music":{"video":'rand_ind:css selector;ytd-video-renderer'
                                },
                       "gaming":{"video":"rand_ind:css selector;ytd-video-renderer"
-                          
+                    
+
+                        
                       },
                   }
                  }
